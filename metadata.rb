cab16bdeab9cebf5d2088ade4b9e113afc526c8f
Benson Miller <benson.miller@gmail.com>
2014-09-03 13:37:55 -0700

    Adding chef-sugar dependency to metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ version '1.2.2'
 
 # for secrets management in setup_auth recipe
 depends 'chef-vault', '>= 1.0.4'
+
+# For sugary 'encrypted_data_bag_item_for_environment' method
+depends 'chef-sugar', '>= 2.0.0'
